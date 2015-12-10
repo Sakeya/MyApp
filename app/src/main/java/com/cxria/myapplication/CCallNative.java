8eef9c92a6f997101faf8b74612eab4bbe429715
Sakeya <v4xiaoyu@163.com>
2015-12-10 18:38:59 +0800

    15th

--- app/src/main/java/com/cxria/myapplication/CCallNative.java
+++ app/src/main/java/com/cxria/myapplication/CCallNative.java
@@ -4,6 +4,9 @@ package com.cxria.myapplication;
  * Created by xiaoyu-liu on 15-12-10.
  */
 public class CCallNative {
+    static {
+        System.loadLibrary("CallNative");
+    }
     private static CCallNative mNative = new CCallNative();
     public static CCallNative instance(){return mNative;}
 
